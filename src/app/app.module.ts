77c39c3a735654c840b592fa152ae22f55aad757
Beruto <beruto000@gmail.com>
2018-03-15 09:34:45 +0100

    First commit, Material angular and animated.css installed, routerlink installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', redirectTo: 'login'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
