603eb696be93b1ee46d73b231134c725337c57c5
Beruto <beruto000@gmail.com>
2018-03-20 21:57:17 +0100

    register component finalizado, queda el servicio

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatInput} from '@angular/material';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  registerFormControl: FormGroup;
+
+  constructor() {
   }
 
+  ngOnInit() {
+    const MIN_CHARS = 0;
+    this.registerFormControl = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(MIN_CHARS)]),
+      surname: new FormControl('', [Validators.required, Validators.minLength(MIN_CHARS)]),
+      email: new FormControl('', [Validators.required, Validators.minLength(MIN_CHARS), Validators.email]),
+      username: new FormControl('', [Validators.required, Validators.minLength(MIN_CHARS)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(MIN_CHARS)]),
+      rePassword: new FormControl('', [Validators.required, Validators.minLength(MIN_CHARS)]),
+    });
+  }
+
+  showPassword(anInput: MatInput) {
+    if (anInput.type === 'password') {
+      anInput.type = 'text';
+    } else {
+      anInput.type = 'password';
+    }
+  }
 }
