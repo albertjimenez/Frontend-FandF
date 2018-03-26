4230106f137eff89c88f6379590ebf2912341c3a
kaiser95 <al288353@uji.es>
2018-03-26 19:13:47 +0200

    Input simple con autocompletado

--- src/app/gmaps/gmaps.component.ts
+++ src/app/gmaps/gmaps.component.ts
@@ -1,40 +1,31 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {GooglePlaceDirective} from 'ngx-google-places-autocomplete';
-import {ComponentRestrictions} from 'ngx-google-places-autocomplete/objects/options/componentRestrictions';
 import {Address} from 'ngx-google-places-autocomplete/objects/address';
+import {ComponentRestrictions} from 'ngx-google-places-autocomplete/objects/options/componentRestrictions';
 
 
 @Component({
   selector: 'app-gmaps',
-  templateUrl: './gmaps.component.html',
-  styleUrls: ['./gmaps.component.css']
+  template: `<input ngx-google-places-autocomplete [options]="{
+    types: ['establishment'],
+    componentRestrictions: { country: 'ES'}
+    }" #places="ngx-places" (onAddressChange)="onChange($event)" />`,
 })
 export class GmapsComponent implements OnInit {
 
+
   @ViewChild('place') place: GooglePlaceDirective;
 
+  options = {
+    types: ['establishment'],
+    componentRestrictions: { country: 'ES'}
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
   public onChange(address: Address) {
-    console.log(address.geometry.location.lng());
-    console.log(address.geometry.location.lat());
-    console.log(address.geometry.location.toJSON());
-    console.log(address.geometry.viewport.getNorthEast());
-    console.log(address.reviews);
-  }
-
-  public changeConfig() {
-    this.place.options.componentRestrictions = new ComponentRestrictions({
-      country: 'UA'
-    });
-
-    this.place.reset();
-  }
-
-  public search() {
 
   }
 
