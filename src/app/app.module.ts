29a5ea3d5f1ccd85d7d814d0c5606233c79f2509
kaiser95 <al288353@uji.es>
2018-03-26 18:10:03 +0200

    Primer commit a gmaps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {FooterComponent} from './footer/footer.component';
 import {LoginService} from './login/login.service';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
+import { GmapsComponent } from './gmaps/gmaps.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -29,7 +30,8 @@ export const routes: Routes = [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
+    GmapsComponent
   ],
   imports: [
     BrowserModule,
