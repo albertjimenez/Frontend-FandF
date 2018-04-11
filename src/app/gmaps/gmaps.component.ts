08029b7e5b7fc9beb167bd0eecba71a5269f3aa9
kaiser95 <al288353@uji.es>
2018-04-11 11:10:25 +0200

    Añadido el horario a la información general

--- src/app/gmaps/gmaps.component.ts
+++ src/app/gmaps/gmaps.component.ts
@@ -11,9 +11,15 @@ import {Address} from 'ngx-google-places-autocomplete/objects/address';
 export class GmapsComponent implements OnInit {
   tempAddress = null;
   hours = [];
+  hour = '';
   rating;
   state;
   stars = [];
+  period_one = [['', '']];
+  period_two = [['', '']];
+  only_one = false;
+  is_two_empty = false;
+  index = 0;
 
   week = [
     {value: '1', viewValue: 'Lunes'},
@@ -30,8 +36,11 @@ export class GmapsComponent implements OnInit {
 
   }
   onChange(address: Address) {
-    // this.number = address.reviews.length;
         this.tempAddress = address;
+        this.hours = [];
+        this.period_one = [];
+        this.period_two = [];
+        this.stars = [];
         if (address.opening_hours.open_now) {
           this.state = 'Abierto';
         } else {
@@ -39,12 +48,41 @@ export class GmapsComponent implements OnInit {
         }
         console.log(address.opening_hours);
         for (let  i = 0; i < address.opening_hours.weekday_text.length; i++) {
-          this.hours.push(address.opening_hours.weekday_text[i].split(': '));
+          this.hours.push([this.week[i].viewValue, address.opening_hours.weekday_text[i].split(': ')[1]]);
         }
         this.rating = address.rating.toFixed();
         for (let i = 0; i < this.rating; i++) {
           this.stars.push(i);
         }
+        console.log(this.hours);
+        this.hour = this.hours[0][1];
+        console.log(this.hour);
+        for (this.index = 0; this.index < this.hours.length; this.index++) {
+            if (this.hours[this.index][1] === this.hour) {
+              this.period_one.push(this.hours[this.index]);
+            } else {
+              break;
+            }
+        }
+        if (this.index !== this.hours.length) {
+            for (this.index; this.index < this.hours.length; this.index++) {
+              if (this.hours[this.index][1] === 'Closed') {
+                this.period_two.push([this.hours[this.index][0], 'Cerrado']);
+              } else {
+                this.period_two.push(this.hours[this.index]);
+              }
+
+            }
+          if (this.period_two.length === 1) {
+            this.only_one = true;
+          }
+        } else {
+          this.is_two_empty = true;
+        }
+
+        console.log(this.only_one);
+        console.log(this.period_one);
+        console.log(this.period_two);
   }
 
 }
