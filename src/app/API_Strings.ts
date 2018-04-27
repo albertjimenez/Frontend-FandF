db4231f7aec3aee623cf658433b52be168ada27f
Beruto <beruto000@gmail.com>
2018-04-27 11:34:30 +0200

    new url WSS

--- src/app/API_Strings.ts
+++ src/app/API_Strings.ts
@@ -1,8 +1,8 @@
-const API = 'http://localhost:4567/api/';
-// const API = 'https://foodandfriendsapp.herokuapp.com/api/';
+// const API = 'https://localhost:4567/api/';
+const API = 'https://foodandfriendsapp.herokuapp.com/api/';
 export const login_endpoint = API + 'login';
 export const register_endpoint = API + 'register';
 export const home_endpoint = API + 'home';
 export const fetch_email = API + 'fetch_email';
-// export const WS_HELLO = 'ws://localhost:4567/hello';
-export const WS_HELLO = 'wss://foodandfriendsapp.herokuapp.com/hello';
+// export const WS_HELLO = 'wss://localhost:4567/api/hello';
+export const WS_HELLO = 'wss://foodandfriendsapp.herokuapp.com/api/hello';
