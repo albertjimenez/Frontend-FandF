7d34db990d923a3e457370cf02d5d815287f5bb3
kaiser95 <al288353@uji.es>
2018-05-08 18:43:26 +0200

    Gmaps: arreglado estilo Buscar amigos: prueba de filtro

--- src/app/general-friends/friend-search/assistant/assistant.component.ts
+++ src/app/general-friends/friend-search/assistant/assistant.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import {startWith} from 'rxjs/operators/startWith';
+import {map} from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-assistant',
@@ -7,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssistantComponent implements OnInit {
 
+  myControl: FormControl = new FormControl();
+
+  dbNames = [
+    'Santi',
+    'Berbel',
+    'Peris',
+    'Tokeisi'
+  ];
+
+  dbUsers = [];
   numMatches = 0;
+  filteredOptions: Observable<string[]>;
   constructor() { }
 
   ngOnInit() {
+    const friend = {
+      name: 'Peris',
+      image: '../../../assets/peris.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    const friend2 = {
+      name: 'Santi',
+      image: '../../../assets/santi.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    const friend3 = {
+      name: 'Berbel',
+      image: '../../../assets/berbel.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    const friend4 = {
+      name: 'Tokeisi',
+      image: '../../../assets/tokeisi.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    this.dbUsers.push(friend, friend2, friend3, friend4);
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(val => this.filter(val))
+      );
   }
 
+  filter(val: string): string[] {
+    return this.dbNames.filter(option =>
+      option.toLowerCase().indexOf(val.toLowerCase()) === 0);
+  }
 }
