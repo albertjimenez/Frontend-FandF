ace442b145058649579ccc474117c962c7bc8162
kaiser95 <al288353@uji.es>
2018-05-08 17:45:49 +0200

    Filtro en interfaz listar amigos

--- src/app/general-friends/general-friends.component.ts
+++ src/app/general-friends/general-friends.component.ts
@@ -13,11 +13,42 @@ export class GeneralFriendsComponent implements OnInit {
 
   ngOnInit() {
     const friend = {
-      name: 'Raúl Peris',
+      name: 'Peris',
       image: '../../../assets/peris.jpg',
       lastConexion: 'Amigos desde el 29/10/2017'
     };
-    this.friendsList.push(friend, friend, friend, friend);
+    const friend2 = {
+      name: 'Santi',
+      image: '../../../assets/santi.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    const friend3 = {
+      name: 'Berbel',
+      image: '../../../assets/berbel.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    const friend4 = {
+      name: 'Tokeisi',
+      image: '../../../assets/tokeisi.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    this.friendsList.push(friend, friend2, friend3, friend4);
+  }
+
+  filterElems(filter: string) {
+    this.numMatches = 0;
+    let ul, li, i, card;
+    ul = document.getElementById('myUL');
+    li = ul.getElementsByTagName('mat-card-title');
+    card = ul.getElementsByTagName('mat-card');
+    for (i = 0; i < li.length; i++) {
+      if (li[i].innerHTML.toLocaleLowerCase().indexOf(filter) > -1) {
+        card[i].style.display = '';
+        this.numMatches++;
+      } else {
+        card[i].style.display = 'none';
+      }
+    }
   }
 
 
