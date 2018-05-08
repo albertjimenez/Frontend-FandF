298f8b7fc4257e8701199206fd6eef196654ae4b
kaiser95 <al288353@uji.es>
2018-05-08 17:30:33 +0200

    Vista previa interfaz listar amigos

--- src/app/general-friends/general-friends.component.ts
+++ src/app/general-friends/general-friends.component.ts
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralFriendsComponent implements OnInit {
 
+  friendsList = [];
   numMatches = 0;
   constructor() { }
 
   ngOnInit() {
+    const friend = {
+      name: 'Raúl Peris',
+      image: '../../../assets/peris.jpg',
+      lastConexion: 'Amigos desde el 29/10/2017'
+    };
+    this.friendsList.push(friend, friend, friend, friend);
   }
 
 
